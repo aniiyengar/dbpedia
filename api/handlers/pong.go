87c0e1563fb3ef02179ae27262f697a38ac08917
Aniruddh Iyengar <aniiyengar@berkeley.edu>
2018-08-15 10:19:09 -0700

    Remove useless log

--- api/handlers/pong.go
+++ api/handlers/pong.go
@@ -6,17 +6,11 @@ package handlers
 import (
     "fmt"
     "net/http"
-
-    "github.com/aniiyengar/dbpedia/api/dropbox"
-    "github.com/aniiyengar/dbpedia/api/utils"
 )
 
 type PongHandler struct {}
 
 func (h PongHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    shards, _ := dropbox.GetPageShards("satty")
-    page, _ := dropbox.GetPageFromShards(shards)
-    utils.Debug(page)
     if r.URL.Path == "/api/ping" {
         // Signal that we're live
         fmt.Fprintf(w, "pong")
